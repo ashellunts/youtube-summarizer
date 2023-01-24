bc6a956a5eb83ecc749a15ce84690298f6041467
Artur Shellunts <shellunts.artur@gmail.com>
2023-01-24 20:47:42 +0100

    use dotenv to load api key in tests

--- tests/test_make_summary.py
+++ tests/test_make_summary.py
@@ -1,6 +1,16 @@
+import os
+import openai
+import pytest
+from dotenv import load_dotenv
 from src import summary
 
 
 def _test_make_summary():
     s = summary.make("Hello my name is Alex and I am software engineer. I make apps and sites.")
     assert "software engineer" in s and len(s) > 100
+
+
+@pytest.fixture(autouse=True)
+def init_openapi():
+    load_dotenv()
+    openai.api_key = os.getenv("OPENAPI_KEY")
