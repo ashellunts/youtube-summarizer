0805ce9dc606ef3ee92f80ce4f3a69ccc7261ff8
Artur Shellunts <shellunts.artur@gmail.com>
2023-04-15 21:20:43 +0200

    remove mock summarizer

--- src/app.py
+++ src/app.py
@@ -3,20 +3,14 @@ from . import summary, video_id
 from . import transcription
 
 app = Flask('app')
-summarizer = summary
-
-
-def set_mock_summarizer(mock_summarizer):
-    global summarizer
-    summarizer = mock_summarizer
 
 
 @app.route('/api', methods=['POST'])
-def make_summary_2():
+def make_summary():
     video_url = request.args.get('video_url')
     id = video_id.get_from_url(video_url)
     _, transcript = transcription.get_english_transcription(id)
-    return summarizer.make(transcript)
+    return summary.make(transcript)
 
 
 def get_server():
