caf458ae58b72aec55ae34b4bbef4767af78a1bf
Artur Shellunts <shellunts.artur@gmail.com>
2023-04-15 21:20:43 +0200

    fix api test

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,8 +1,8 @@
 import requests
 
 
-def _test_make_summary_api():
+def test_make_summary_api():
     response = requests.get(
         "http://localhost:8080/api?video_url=https://www.youtube.com/watch?v=x7KedT6uvus",
     )
-    assert response.text == "hello world"
+    assert "software" in response.text and "Germany" in response.text
