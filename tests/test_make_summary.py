60f51c5c47cd5db07b34304d3443adb8be425048
Artur Shellunts <shellunts.artur@gmail.com>
2023-04-15 21:20:43 +0200

    remove dotenv usage, openAI key should be set in env variables

--- tests/test_make_summary.py
+++ tests/test_make_summary.py
@@ -1,14 +1,4 @@
-import os
-import openai
-import pytest
-from dotenv import load_dotenv
 from src import summary
-
-
-@pytest.fixture(autouse=True)
-def init_openapi():
-    load_dotenv()
-    openai.api_key = os.getenv("OPENAPI_KEY")
 
 
 def test_make_summary_gpt_3_5_turbo(check_openai_tests):
