ae9c9fa9edc1984d750585be14465b7b5ba1b07c
Artur Shellunts <shellunts.artur@gmail.com>
2023-04-15 21:20:43 +0200

    remove older tests that used separate flask process

--- tests/test_in_place.py
+++ tests/test_in_place.py
@@ -8,8 +8,24 @@ def test_client():
         yield test_client
 
 
-def test_in_place(test_client):
+def test_make_summary_api_1(test_client, check_openai_tests):
     query_string = {'video_url': 'https://www.youtube.com/watch?v=x7KedT6uvus'}
     response = test_client.post('/api', query_string=query_string)
     assert response.status_code == 200
     assert 'shields' in response.text.lower()
+
+
+def test_make_summary_api_video_that_needs_2_api_calls(test_client, check_openai_tests):
+    query_string = {'video_url': 'https://www.youtube.com/watch?v=FrMRyXtiJkc'}
+    response = test_client.post('/api', query_string=query_string)
+    assert response.status_code == 200
+    assert "vim" in response.text.lower()
+
+
+def test_make_summary_api_video_that_needs_tldr(check_openai_tests, test_client):
+    query_string = {'video_url': 'https://www.youtube.com/watch?v=STpbPXW9-pA'}
+    response = test_client.post('/api', query_string=query_string)
+    assert response.status_code == 200
+    assert "mcconnell" in response.text.lower()
+    assert "tldr" in response.text.lower()
+    assert "longer version" in response.text.lower()
