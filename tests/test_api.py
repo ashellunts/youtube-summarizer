10b54d923d72514b917d0745586f1636435d9efe
Artur Shellunts <shellunts.artur@gmail.com>
2023-04-15 21:20:43 +0200

    mark new test with openai fixture

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,7 +1,7 @@
 import requests
 
 
-def test_make_summary_api():
+def test_make_summary_api(check_openai_tests):
     response = requests.get(
         "http://localhost:8080/api?video_url=https://www.youtube.com/watch?v=x7KedT6uvus",
     )
