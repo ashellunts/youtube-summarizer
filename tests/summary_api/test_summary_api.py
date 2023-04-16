94b7b7364c66cbae1fee6944a33d01a2048481b6
Artur Shellunts <shellunts.artur@gmail.com>
2023-04-16 14:34:34 +0200

    add dumping of actual response in test_summary_api

--- tests/summary_api/test_summary_api.py
+++ tests/summary_api/test_summary_api.py
@@ -1,6 +1,6 @@
 import pytest
 from os.path import join
-from helpers import read_file
+from helpers import read_file, write_file
 
 
 @pytest.mark.parametrize("video_id", [
@@ -12,4 +12,6 @@ def test_make_summary(test_client, video_id, test_dir_path):
     response = test_client.post('/summary', query_string=query_string)
     assert response.status_code == 200
     expected = read_file(join(test_dir_path, f"expected/{video_id}_summary.html"))
-    assert expected == response.text
+    if expected != response.text:
+        write_file(join(test_dir_path, f"_actual.html"), response.text)
+        assert False, f"The actual response does not match the expected (expected/{video_id}_summary.html). See _actual.html for details."
