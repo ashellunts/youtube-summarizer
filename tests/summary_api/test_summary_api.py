f7ee60e17906ab8f30626a2ceb5142fd7d66496e
Artur Shellunts <shellunts.artur@gmail.com>
2023-04-16 14:47:30 +0200

    better error message

--- tests/summary_api/test_summary_api.py
+++ tests/summary_api/test_summary_api.py
@@ -11,7 +11,9 @@ def test_summary_api(test_client, video_id, test_dir_path):
     query_string = {'video_url': f'https://www.youtube.com/watch?v={video_id}'}
     response = test_client.post('/summary', query_string=query_string)
     assert response.status_code == 200
-    expected = read_file(join(test_dir_path, f"expected/{video_id}_summary.html"))
+    expected_filename = join(test_dir_path, f"expected/{video_id}_summary.html")
+    expected = read_file(expected_filename)
     if expected != response.text:
-        write_file(join(test_dir_path, f"_actual.html"), response.text)
-        assert False, f"The actual response does not match the expected (expected/{video_id}_summary.html). See _actual.html for details."
+        actual_path = join(test_dir_path, f"_actual_{video_id}.html")
+        write_file(actual_path, response.text)
+        assert False, f"The actual response({actual_path}) does not match the expected ({expected_filename})"
