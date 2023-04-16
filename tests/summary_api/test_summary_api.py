3b1ae85efda950026ff8caa6e31f270edbf9c9d1
Artur Shellunts <shellunts.artur@gmail.com>
2023-04-16 14:34:47 +0200

    rename test

--- tests/summary_api/test_summary_api.py
+++ tests/summary_api/test_summary_api.py
@@ -7,7 +7,7 @@ from helpers import read_file, write_file
     ("x7KedT6uvus"), ("FrMRyXtiJkc"), ("STpbPXW9-pA")
 ])
 @pytest.mark.vcr()
-def test_make_summary(test_client, video_id, test_dir_path):
+def test_summary_api(test_client, video_id, test_dir_path):
     query_string = {'video_url': f'https://www.youtube.com/watch?v={video_id}'}
     response = test_client.post('/summary', query_string=query_string)
     assert response.status_code == 200
