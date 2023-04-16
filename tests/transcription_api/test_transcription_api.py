d7bb09ed2f1cbd8a9c6eb764056c44e43c8af346
Artur Shellunts <shellunts.artur@gmail.com>
2023-04-16 15:25:59 +0200

    better error message

--- tests/transcription_api/test_transcription_api.py
+++ tests/transcription_api/test_transcription_api.py
@@ -9,7 +9,9 @@ def test_transcription_api(test_client, test_dir_path):
     query_string = {'video_url': f'https://www.youtube.com/watch?v={video_id}'}
     response = test_client.post('/transcription', query_string=query_string)
     assert response.status_code == 200
-    expected = read_file(join(test_dir_path, f"expected.html"))
+    expected_filename = join(test_dir_path, f"expected.html")
+    expected = read_file(expected_filename)
     if expected != response.text:
-        write_file(join(test_dir_path, f"_actual.html"), response.text)
-        assert False, "The actual response does not match the expected response. See actual.html for details."
+        actual_filename = join(test_dir_path, f"_actual.html")
+        write_file(actual_filename, response.text)
+        assert False, f"The actual response {actual_filename} does not match the expected {expected_filename}. See actual.html for details."
